75de7863da20f679707209a40a237c6cff5ecc6d
Matt Cirkovic <matthewcirkovic@gmail.com>
2024-05-15 12:08:19 -0600

    Cycling through titles

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var titles = [
     'web designer',
     'disc golfer',
+    'data scientist',
+    'bad golfer',
     'trail runner',
+    'data engineer',
+    'IT professional',
     'software developer'
 ];
 
+var i = 0;
+
+setInterval(() => {
+    console.log(i);
+    document.getElementById('home-title').innerHTML = titles[i % titles.length];
+    i++
+}, 1000);
